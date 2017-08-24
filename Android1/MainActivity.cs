aceab6756a168ce685f26589ff67f8a5c5905f21
Stevens <dstevens@kforce.com>
2017-08-24 10:21:34 -0400

    Add project files.

---
+++ Android1/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace Android1
+{
+    [Activity(Label = "Android1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            MobileCenter.Start("441ede81-2bc7-4c29-80e8-a810ed61791b",
+                   typeof(Analytics), typeof(Crashes));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
